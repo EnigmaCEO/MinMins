82b90c7c8e5031b3e2c44e933889c09ae43076dd
Juan Barba <juan.barba.o@gmail.com>
2019-06-27 22:44:29 -0500

    Added new assets and widgets to the login and register screens, compiled all into a prefab, created script to start doing some behaviors on pressing buttons, finished basic form building, toggle needs other textures yet

---
+++ Assets/A_Game/Components/EnjinLogin/EnjinLogin.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnjinLogin : MonoBehaviour
+{
+    public void closeDialog() {
+        gameObject.SetActive(false);
+    }
+}
