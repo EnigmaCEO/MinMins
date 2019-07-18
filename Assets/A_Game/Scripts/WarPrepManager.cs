67209668820adeee6fea5a7df77444bc81929529
Xavier Moore <bass901@aol.com>
2019-07-18 02:09:20 -0400

    Importing Photon

--- Assets/A_Game/Scripts/WarPrepManager.cs
+++ Assets/A_Game/Scripts/WarPrepManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Enigma.CoreSystems;
 
 public class WarPrepManager : MonoBehaviour
 {
--- Assets/EnigmaCoreSystems/Scripts/Misc.cs
+++ Assets/EnigmaCoreSystems/Scripts/Misc.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using Enigma.CoreSystems;
 
 public class Misc
 {
